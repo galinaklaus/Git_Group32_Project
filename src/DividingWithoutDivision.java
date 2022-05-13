c0d0a1ca6b3eb99c2e5aa0ab37fba14ca0ceb1b4
zivadinjo <zivadinjo1985@gmail.com>
2022-05-13 13:06:05 -0500

    Dividing Without Division

--- src/DividingWithoutDivision.java
+++ src/DividingWithoutDivision.java
@@ -5,12 +5,12 @@ public class DividingWithoutDivision {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
-        int b = input.nextInt();
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
         int result = 0;
 
-        while (a >= b){
-            a = a-b;
+        while (num1 >= num2) {
+            num1 = num1 - num2;
             result++;
         }
         System.out.println(result);
