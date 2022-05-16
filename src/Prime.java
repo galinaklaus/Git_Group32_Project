fac8e80c2d695c62cf5884bb9479efe8869a29a0
Htopcuoglu123 <httopcuoglu@gmail.com>
2022-05-16 17:37:53 -0400

    Created Prime Task

---
+++ src/Prime.java
@@ -0,0 +1,25 @@
+public class Prime {
+
+    public class PrimeNum {
+        public static String isPrime(int num){
+            String s="";
+            int count=0;
+            for (int i = 2; i <num/2 ; i++) {
+                if(num%i==0){
+                    count++;
+                }
+            }
+            if(count==1){
+                s+=num+"is Prime number";
+            }else{
+                s+=num+"is not prime number";
+            }
+            return s;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(isPrime(23));
+        }
+    }
+
+}
