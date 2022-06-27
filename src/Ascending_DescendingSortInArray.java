ef8c07bc835e0e21593ce48efb3acc8009f1cd3f
zivadinjo <zivadinjo1985@gmail.com>
2022-06-27 18:00:21 -0500

    6/27

--- src/Ascending_DescendingSortInArray.java
+++ src/Ascending_DescendingSortInArray.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Ascending_DescendingSortInArray {
+//public class Ascending_DescendingSortInArray {
 
 /*
  Array - Sort Ascending and Descending
@@ -10,42 +10,42 @@ Ex:
              arr = Sort(arr);         ==>{ 7, 8, 9, 10};
 
  */
-
-    public class SortAscendingInArray {
-        public static void main(String[] args) {
-            int[] nums = {2, 4, 7, 1, 6, 9};
-            System.out.println(Arrays.toString(sortAscending(nums)));
-            System.out.println(Arrays.toString(sortDescending(nums)));
-        }
-
-        public static int[] sortAscending(int[] nums) {
-            int max = 0;
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[i] > nums[j]) {
-                        max = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = max;
-                    }
-                }
-            }
-            return nums;
-        }
-
-
-        public static int[] sortDescending(int[] nums) {
-            int min = 0;
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[i] < nums[j]) {
-                        min = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = min;
-                    }
-                }
-
-            }
-            return nums;
-        }
-    }
-}
+//
+//    public class SortAscendingInArray {
+//        public static void main(String[] args) {
+//            int[] nums = {2, 4, 7, 1, 6, 9};
+//            System.out.println(Arrays.toString(sortAscending(nums)));
+//            System.out.println(Arrays.toString(sortDescending(nums)));
+//        }
+//
+//        public static int[] sortAscending(int[] nums) {
+//            int max = 0;
+//            for (int i = 0; i < nums.length; i++) {
+//                for (int j = i + 1; j < nums.length; j++) {
+//                    if (nums[i] > nums[j]) {
+//                        max = nums[i];
+//                        nums[i] = nums[j];
+//                        nums[j] = max;
+//                    }
+//                }
+//            }
+//            return nums;
+//        }
+//
+//
+//        public static int[] sortDescending(int[] nums) {
+//            int min = 0;
+//            for (int i = 0; i < nums.length; i++) {
+//                for (int j = i + 1; j < nums.length; j++) {
+//                    if (nums[i] < nums[j]) {
+//                        min = nums[i];
+//                        nums[i] = nums[j];
+//                        nums[j] = min;
+//                    }
+//                }
+//
+//            }
+//            return nums;
+//        }
+//    }
+//}
